7e7bba90a02bb7d2ea2f5720c0ecf555cc8f13f3
Mohd Tayyab Saeed <m.tayyab.saeed@gmail.com>
2024-11-02 15:27:50 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+function calculate() {
+    let display = document.getElementById("display");
+    display.value = eval(display.value);
+}
